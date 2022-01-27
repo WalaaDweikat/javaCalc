3288267ab32199e0dfbead0a7985bd2de94e651e
Walaa Dweikat <walaadweikt3@gmail.com>
2022-01-27 08:53:24 +0200

    afs

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
 
     public static void main(String args[]){
-        double x = 7;
-        double y = 8;
-        System.out.println(add(x,y));
+
     }
 
-    private static double add(double x, double y){
-        return x+y;
-    }
+
 
 
 }
