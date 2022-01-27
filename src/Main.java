4434988855e7801fd429fdb885145dd80dd65449
Walaa Dweikat <walaadweikt3@gmail.com>
2022-01-27 09:07:31 +0200

    delete add function

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,9 @@ public class Main {
     public static void main(String args[]){
         double x = 7;
         double y = 8;
-        System.out.println(add(x,y));
     }
 
-    private static double add(double x, double y){
-        return y+x;
-    }
+
 
 
 }
